853bcd1242e467da938e0ae7a68abefc53c75dc5
reece-elder <reece.elder@qa.com>
2023-01-16 10:32:51 +0000

    adding basic code

---
+++ code/001-intro/demo.py
@@ -0,0 +1,41 @@
+# We know it runs top to bottom
+print("Hello!")
+print("1")
+print("2")
+print("3")
+
+# Numbers
+tiers = 2     # int
+price = 8.99  # float
+
+# Strings
+flavour  = "Black forest"
+customer = 'Reece'
+messafe  = 'print("Hello")'
+
+# Booleans
+decorated = True
+frosting = False
+
+# Arrays
+decorations = ["candles", "frosting", "sprinkles"]
+print(customer)
+print(tiers)
+print(decorations[1])
+print(decorations[-1])
+
+# naming conventions
+best_practice = "lower case and _"
+camelCase = "lowercase and Upper"
+badpracticenameconvention = "don't do this"
+# print = "A4 paper, don't do this"
+# percentage% = 30
+
+fav_colour = input("Please enter fav colour: ")
+print(fav_colour)
+
+# Casting our number as an integer
+fav_number = int(input("Please enter fav num: "))
+print(fav_number + 1)
+
+print(decorations)
