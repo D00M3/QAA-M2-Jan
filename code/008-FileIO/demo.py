704ed4de4d99bdae56f5be206acc764827ea9038
reece-elder <reece.elder@qa.com>
2023-01-18 11:33:09 +0000

    file-io

---
+++ code/008-FileIO/demo.py
@@ -0,0 +1,12 @@
+# File IO Input Output 
+# Using Python to read from files, do x with the data and modify the data in the files
+# Python can be used to change .txt, .csv, .MD, 
+
+# Opening the file and storing it as a variable fruit.txt
+fruit_file = open("fruit.txt")
+fruit_line = fruit_file.readline() # Reading the first line of the file
+print(fruit_line)
+
+# Exercise - Create a file called colours.txt that contains 
+# 5 colours and use Python to read and print them
+# Stretch goal - Use python to print each colour on a different line 
