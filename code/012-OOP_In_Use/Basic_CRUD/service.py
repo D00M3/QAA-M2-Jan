61390d1997462347f38c88de8a55355ed6d705ad
reece-elder <reece.elder@qa.com>
2023-01-20 12:25:04 +0000

    service stuff

--- code/012-OOP_In_Use/Basic_CRUD/service.py
+++ code/012-OOP_In_Use/Basic_CRUD/service.py
@@ -17,7 +17,7 @@ print(service1.createMovie(4, "StarShip Troopers 4", 70, "Sci_Fi, Action"))
 
 # Service as a file
 
-def createMovie(self, movie_id, title, runtime, genre):
+def createMovie(movie_id, title, runtime, genre):
     movieObj = movie(movie_id, title, runtime, genre)
     return(movieObj)
 
