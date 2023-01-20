ed3a3e2b4a37b2959f04189ffd7aea400a1aa131
reece-elder <reece.elder@qa.com>
2023-01-20 14:50:07 +0000

    search query

--- code/013-Flask/demo.py
+++ code/013-Flask/demo.py
@@ -12,7 +12,23 @@ app = Flask(__name__)
 
 @app.route("/hello")
 def hello():
-    return "Whaytever text we send this will work"
+    return "make a second route that listens for /html and sends a h2 header"
+
+@app.route("/html")
+def html():
+    return "<h2> Hello World! </h2>"
+
+@app.route("/image")
+def html_2():
+    # return -> Outputting some data back to the browser
+    return "<img src='https://picsum.photos/200/300' alt='random pic'>"
+
+# Url Query, Param queries, Body Requests
+@app.route("/query/<search>")
+def search_query(search):
+    # data = db.query("SELECT * FROM data")
+    # return data
+    return f"You have searched for {search}.. Sorry, no results :( "
 
 
 # To run the app:
